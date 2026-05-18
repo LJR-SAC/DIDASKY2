4e48e4d0cd4d12a7379529a087e30d1c3e0781ac
LJR-SAC <e25.joserodrigo.moralesc@suizoamericano.edu.gt>
2026-05-18 10:31:07 -0600

    Reduce max_tokens from 1500 to 1000

--- api/chat.js
+++ api/chat.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
                     { role: 'user', content: prompt }
                 ],
                 temperature: 0.7,
-                max_tokens: 1500,
+                max_tokens: 1000,
                 stream: false
             })
         });
